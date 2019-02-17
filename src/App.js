1810442b030666bb3ffeeca1a40395254a19f3a9
Stuart Rendall <14021635@uhi.ac.uk>
2019-02-17 12:06:49 +0000

    people in array, bootstrap styling ListPeople class

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React, { Component } from 'react';
+import ListPeople from './ListPeople';
 
-class PeopleList extends React.Component {
-  render(){
-    const people= this.props.people
-
-    return <ol>
-    {people.map((person) =>
-      <li key ={person.name}>{person.name}</li>
-    )}
-    </ol>
-  }
-
-}
+const people = [
+    {
+      id: 'russell',
+      name: 'Russell Hunter',
+      handle: '@russell_hunter',
+    },
+    {
+      id:'derek',
+      name:'Derek Summers',
+      handle: '@derek_summers'
+    },
+    {
+      id:'gill',
+      name:'Gillian Douglas',
+      handle: '@gill_douglas'
+    },
+    {
+      id:'stuart',
+      name:'Stuart Rendall',
+      handle: '@stuart_rendall'
+    }
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PeopleList people={[
-          {name: 'Russell'},
-          {name: 'Malcolm',},
-          {name: 'Magnus',}]}
-        />
-        <PeopleList people={[
-          {name: 'Gill '},
-          {name: 'Sarah',},
-          {name: 'Grace',}]}
-        />
-        <PeopleList people={[
-          {name: 'Jenny'},
-          {name: 'Helen',}]}
-        />
+      <div className="container">
+        App.js: App class called
+        <ListPeople people={people} />
       </div>
     );
   }
---
+++ src/ListPeople.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+class ListPeople extends Component {
+  render(){
+    console.log('Props', this.props)
+    return (
+        <ol>
+          ListPeople.js: ListPeople class called<br/>
+          {this.props.people.map((person)=>(
+            <div key={person.id}>
+              <div className="card text-center">
+                <div className="card-body">
+                  <h5 className="card-title">{person.name}</h5>
+                  <p className="card-text">{person.handle}</p>
+                  <button type="button" className="btn btn-dark">X</button>
+                </div>
+              </div>
+              <br/>
+            </div>
+          ))}
+        </ol>
+    )
+  }
+}
+
+export default ListPeople
