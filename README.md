c48b9e2816c50187f2ad5b42205f609229e6f301
Stuart Rendall <14021635@uhi.ac.uk>
2019-02-17 14:00:41 +0000

    List filter added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#WebDevPractical
+# WebDevPractical
 Practical work for Web Development Module
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/ListPeople.js
+++ src/ListPeople.js
@@ -1,19 +1,62 @@
 import React, {Component} from 'react'
 
 class ListPeople extends Component {
+  state = {
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+  }
+  clearFilter = () =>{
+    this.updateQuery('')
+  }
+
   render(){
-    console.log('Props', this.props)
+    const { query } = this.state
+    const { people, onDeletePerson } = this.props
+    const showingPeople = query === ''
+      ? people
+      : people.filter((p) =>(
+        p.name.toLowerCase().includes(query.toLowerCase())
+      ))
+
     return (
+      <div>
+        <div>
+          <nav className="navbar navbar-light bg-light">
+            <form className="form-inline">
+              <input className ="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Serach"
+                value= {this.state.query}
+                onChange={(event) => this.updateQuery(event.target.value)}
+              />
+              <button className="btn btn-outline-success my-2 my-sm-0"
+                type="submit">Search</button>
+            </form>
+          </nav>
+        </div>
+
+        {showingPeople.length !== people.length &&(
+          <div className="text-center">
+            <span>Now Showing {showingPeople.length} of {people.length}</span>
+            <button onClick={this.clearFilter}>Show all</button>
+          </div>
+        )}
+
         <ol>
           ListPeople.js: ListPeople class called<br/>
-          {this.props.people.map((person)=>(
+          {showingPeople.map((person)=>(
             <div key={person.id}>
               <div className="card text-center">
                 <div className="card-body">
                   <h5 className="card-title">{person.name}</h5>
                   <p className="card-text">{person.handle}</p>
                   <button
-                    onClick={() => this.props.onDeletePerson(person)}
+                    onClick={() => onDeletePerson(person)}
                     type="button" className="btn btn-dark">X</button>
                 </div>
               </div>
@@ -21,6 +64,7 @@ class ListPeople extends Component {
             </div>
           ))}
         </ol>
+      </div>
     )
   }
 }
